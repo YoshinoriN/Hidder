aa95dd0f68ed61c8108438d92bad1a9422cf3a75
Yoshinori.N <nayo-msoft@outlook.jp>
2016-02-14 23:37:53 +0900

    (Issue #14):テキストボックスドラッグでファイルパスを表示するようにした。

---
+++ Hidder/Behavior/TextBoxBehaviorExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Hidder.Behavior
+{
+    /// <summary>
+    /// テキストボックスのビヘイビア拡張
+    /// </summary>
+    public class TextBoxBehaviorExtensions : TextBoxBehavior
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.PreviewDragOver += textBox_PreviewDragOver;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.PreviewDragOver -= textBox_PreviewDragOver;
+        }
+
+        private void textBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effects = DragDropEffects.Copy;
+                var dropFiles = e.Data.GetData(System.Windows.DataFormats.FileDrop) as string[];
+                if (dropFiles == null) return;
+                var textBox = sender as TextBox;
+                textBox.Text = dropFiles[0];
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+            }
+        }
+    }
+}
+
+
